f497c8982abea5c6ef446d29a0912c2a53c8a252
Martin Slota <martin.slota@workday.com>
2024-09-20 11:26:46 +0200

    Add an ESM-only dependency

--- src/resolvers/hello.ts
+++ src/resolvers/hello.ts
@@ -1,3 +1,10 @@
-export function hello() {
+import pTimeout from "p-timeout";
+
+async function slowGreeting() {
+  await new Promise((resolve) => setTimeout(resolve, 500));
   return "Hello world!";
+}
+
+export function hello() {
+  return pTimeout(slowGreeting(), { milliseconds: 1000 });
 }
