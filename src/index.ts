ca3d0de60e2586ddccebfe5d98d16d3dadef8f8e
Martin Slota <martin.slota@workday.com>
2024-09-20 11:24:58 +0200

    Add GraphQL sample server https://graphql.org/graphql-js/running-an-express-graphql-server/

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-const MEANING: number = 42;
+import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { buildSchema } from "graphql";
 
-console.log(`The meaning of life is: ${MEANING}`);
+// The root provides a resolver function for each API endpoint
+import { root } from "./resolvers";
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const app = express();
+
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+// Start the server at port
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
